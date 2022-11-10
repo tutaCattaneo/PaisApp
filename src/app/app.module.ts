bc0e02bd561cbeaddb0992cf5eb9a60941ed6d88
Matias Cattaneo <matiascattaneo500@gmail.com>
2022-11-10 16:24:16 -0300

    Primer Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { PaisModule } from './pais/pais.module';
+import { SharedModule } from './shared/shared.module';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PaisModule,
+    SharedModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
